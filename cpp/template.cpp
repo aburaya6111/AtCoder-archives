8e593505edd091a7298b4a30ade8291d6d30d4ce
aburaya6111 <59960488+aburaya6111@users.noreply.github.com>
2020-06-13 02:24:11 +0900

    modify template

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -41,9 +41,8 @@ bool chmin(T &a, const T &b) {
 #define debug_4(x1, x2, x3, x4) cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << ", " #x3 << ": " << x3 << ", " #x4 << ": " << x4 << endl
 #define debug_5(x1, x2, x3, x4, x5) cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << ", " #x3 << ": " << x3 << ", " #x4 << ": " << x4 << ", " #x5 << ": " << x5 << endl
 #ifdef _DEBUG
-#define debug(...)                                                        \
-    CHOOSE((__VA_ARGS__, debug_5, debug_4, debug_3, debug_2, debug_1, ~)) \
-    (__VA_ARGS__)
+#define debug(...) CHOOSE((__VA_ARGS__, debug_5, debug_4, debug_3, debug_2, debug_1, ~)) \
+(__VA_ARGS__)
 #else
 #define debug(...)
 #endif
@@ -52,7 +51,8 @@ bool chmin(T &a, const T &b) {
 
 void Main() {
     int N;
-    debug(N);
+    cout << N << "\n";
+    cout << flush;
 }
 
 #pragma region main
