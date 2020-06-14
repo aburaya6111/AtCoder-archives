5f41af57978af3e790229e58e5310d27b9a4645a
aburaya6111 <59960488+aburaya6111@users.noreply.github.com>
2020-06-14 14:30:13 +0900

    imos法を学習

---
+++ cpp/tokyokaijou/C.cpp
@@ -0,0 +1,106 @@
+#pragma region Macros
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define int ll
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SIZE(x) ((ll)(x).size())
+#define MAX(x) *max_element(ALL(x))
+#define MIN(x) *min_element(ALL(x))
+
+template <class T>
+bool chmax(T &a, const T &b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+bool chmin(T &a, const T &b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+
+// debug methods
+// usage: debug(x,y);
+#define CHOOSE(a) CHOOSE2 a
+#define CHOOSE2(a0, a1, a2, a3, a4, x, ...) x
+#define debug_1(x1) cout << #x1 << ": " << x1 << endl
+#define debug_2(x1, x2) cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << endl
+#define debug_3(x1, x2, x3) cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << ", " #x3 << ": " << x3 << endl
+#define debug_4(x1, x2, x3, x4) cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << ", " #x3 << ": " << x3 << ", " #x4 << ": " << x4 << endl
+#define debug_5(x1, x2, x3, x4, x5) cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << ", " #x3 << ": " << x3 << ", " #x4 << ": " << x4 << ", " #x5 << ": " << x5 << endl
+#ifdef _DEBUG
+#define debug(...)                                                        \
+    CHOOSE((__VA_ARGS__, debug_5, debug_4, debug_3, debug_2, debug_1, ~)) \
+    (__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+#pragma endregion
+
+void Main() {
+    int N, K;
+    cin >> N >> K;
+    vi lamps(N, 0);
+
+    REP(i, N) {
+        cin >> lamps[i];
+    }
+
+    if (K > 42) {
+        REP(i, N) {
+            cout << N;
+            if (i != N - 1) {
+                cout << " ";
+            }
+        }
+        return;
+    }
+
+    REP(i, K) {
+        vi tmp(N + 1);
+        REP(j, N) {
+            debug(lamps[j]);
+            int start = max(j - lamps[j], (int)0);
+            int end = min(j + lamps[j] + 1, N);
+            tmp[end]--;
+            tmp[start]++;
+        }
+        REP(j, N) {
+            debug(tmp[j]);
+            if (j == 0) {
+                lamps[j] = tmp[j];
+                continue;
+            }
+            lamps[j] = tmp[j] + lamps[j - 1];
+        }
+    }
+
+    REP(i, N) {
+        cout << lamps[i];
+        if (i != N - 1) {
+            cout << " ";
+        }
+    }
+}
+
+#pragma region main
+main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    Main();
+    cout << flush;
+}
+#pragma endregion
