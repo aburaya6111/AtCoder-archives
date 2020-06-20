c6c18df1ce6a5a6c6b214a460c0a70ca32c97590
aburaya6111 <59960488+aburaya6111@users.noreply.github.com>
2020-06-21 03:48:20 +0900

    AGC46 A

--- cpp/dowango/A.cpp
+++ cpp/dowango/A.cpp
@@ -41,8 +41,9 @@ bool chmin(T &a, const T &b) {
 #define debug_4(x1, x2, x3, x4) cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << ", " #x3 << ": " << x3 << ", " #x4 << ": " << x4 << endl
 #define debug_5(x1, x2, x3, x4, x5) cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << ", " #x3 << ": " << x3 << ", " #x4 << ": " << x4 << ", " #x5 << ": " << x5 << endl
 #ifdef _DEBUG
-#define debug(...) CHOOSE((__VA_ARGS__, debug_5, debug_4, debug_3, debug_2, debug_1, ~)) \
-(__VA_ARGS__)
+#define debug(...)                                                        \
+    CHOOSE((__VA_ARGS__, debug_5, debug_4, debug_3, debug_2, debug_1, ~)) \
+    (__VA_ARGS__)
 #else
 #define debug(...)
 #endif
@@ -51,11 +52,12 @@ bool chmin(T &a, const T &b) {
 
 void Main() {
     int N;
-    cout << N << "\n";
+    cin >> N;
+    cout << N / 25 << "\n";
 }
 
 #pragma region main
-signed main() {
+main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     Main();
--- cpp/dowango/B.cpp
+++ cpp/dowango/B.cpp
@@ -41,8 +41,9 @@ bool chmin(T &a, const T &b) {
 #define debug_4(x1, x2, x3, x4) cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << ", " #x3 << ": " << x3 << ", " #x4 << ": " << x4 << endl
 #define debug_5(x1, x2, x3, x4, x5) cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << ", " #x3 << ": " << x3 << ", " #x4 << ": " << x4 << ", " #x5 << ": " << x5 << endl
 #ifdef _DEBUG
-#define debug(...) CHOOSE((__VA_ARGS__, debug_5, debug_4, debug_3, debug_2, debug_1, ~)) \
-(__VA_ARGS__)
+#define debug(...)                                                        \
+    CHOOSE((__VA_ARGS__, debug_5, debug_4, debug_3, debug_2, debug_1, ~)) \
+    (__VA_ARGS__)
 #else
 #define debug(...)
 #endif
@@ -51,11 +52,33 @@ bool chmin(T &a, const T &b) {
 
 void Main() {
     int N;
-    cout << N << "\n";
+    cin >> N;
+    vi K(N - 1);
+    REP(i, N - 1)
+    cin >> K[i];
+
+    vi ans(N, 1);
+
+    for (int i = 1; i < N - 1; i++) {
+        if (K[i - 1] <= K[i]) {
+            ans[i] = K[i - 1];
+        } else {
+            ans[i] = K[i];
+        }
+    }
+    ans[0] = K[0];
+    ans[SIZE(ans) - 1] = K[SIZE(K) - 1];
+
+    REP(i, N) {
+        cout << ans[i];
+        if (i != N - 1) {
+            cout << " ";
+        }
+    }
 }
 
 #pragma region main
-signed main() {
+main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     Main();
